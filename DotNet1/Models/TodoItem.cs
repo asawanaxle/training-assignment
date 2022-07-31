ae057fc2a29833f799f92f3f0a574577e6bd8ec2
Aditi Sawant <Aditi.Sawant@data-axle.com>
2022-07-31 22:01:50 +0530

    updated commit

---
+++ DotNet1/Models/TodoItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace DotNet1.Models
+{
+    public class TodoItem
+    {
+        [Required]
+        [Display(Name = "username")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
+
+
+
